196e86de9da14d4aab1e6e3393d2366bedb2b196
marcelbittrich <marcel.bittrich@gmail.com>
2023-05-23 10:02:32 +0200

    add custom movement logic to movement component

--- Source/DesertSlide/Movement/DesertCharacterMovementComponent.cpp
+++ Source/DesertSlide/Movement/DesertCharacterMovementComponent.cpp
@@ -3,19 +3,99 @@
 
 #include "DesertCharacterMovementComponent.h"
 
-void UDesertCharacterMovementComponent::InitializeComponent()
-{
-	Super::InitializeComponent();
+#include "GameFramework/Character.h"
 
-	UE_LOG(LogTemp, Warning, TEXT("Desert Movement Component Initialized"));
+bool UDesertCharacterMovementComponent::FSavedMove_Desert::CanCombineWith(const FSavedMovePtr& NewMove,
+                                                                          ACharacter* InCharacter, float MaxDelta) const
+{
+	FSavedMove_Desert* NewDesertMove = static_cast<FSavedMove_Desert*>(NewMove.Get());
+
+	// If you start to sprint, you dont want to combine the movements.
+	if (Saved_bWantsToSprint != NewDesertMove->Saved_bWantsToSprint)
+	{
+		return false;
+	}
+	
+	// In case we want to combine, the super function will handle that.
+	return Super::CanCombineWith(NewMove, InCharacter, MaxDelta);
 }
 
-void UDesertCharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-	FActorComponentTickFunction* ThisTickFunction)
+void UDesertCharacterMovementComponent::FSavedMove_Desert::Clear()
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::Clear();
 
-	UE_LOG(LogTemp, Warning, TEXT("Desert Moement Comp Tick"));
+	Saved_bWantsToSprint = 0;
 }
 
+uint8 UDesertCharacterMovementComponent::FSavedMove_Desert::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (Saved_bWantsToSprint)
+	{
+		Result |= FLAG_Custom_0;
+	}
+
+	return Result;
+}
+
+void UDesertCharacterMovementComponent::FSavedMove_Desert::SetMoveFor(ACharacter* C, float InDeltaTime,
+	FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+	Super::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+
+	UDesertCharacterMovementComponent* CharacterMovement = Cast<UDesertCharacterMovementComponent>(C->GetCharacterMovement());
+
+	Saved_bWantsToSprint = CharacterMovement->Safe_bWantsToSprint;
+}
+
+void UDesertCharacterMovementComponent::FSavedMove_Desert::PrepMoveFor(ACharacter* C)
+{
+	Super::PrepMoveFor(C);
+	
+	UDesertCharacterMovementComponent* CharacterMovement = Cast<UDesertCharacterMovementComponent>(C->GetCharacterMovement());
+
+	CharacterMovement->Safe_bWantsToSprint = Saved_bWantsToSprint;
+}
+
+UDesertCharacterMovementComponent::FNetworkPredictionData_Client_Desert::FNetworkPredictionData_Client_Desert(
+	const UCharacterMovementComponent& ClientMovement) : Super(ClientMovement)
+{
+	// Just calling the Super constructor.
+}
+
+FSavedMovePtr UDesertCharacterMovementComponent::FNetworkPredictionData_Client_Desert::AllocateNewMove()
+{
+	return FSavedMovePtr(new FSavedMove_Desert());
+}
+
+FNetworkPredictionData_Client* UDesertCharacterMovementComponent::GetPredictionData_Client() const
+{
+	check(PawnOwner != nullptr)
+
+	if (ClientPredictionData == nullptr)
+	{
+		// To create the prediction data if not available, it has to const_cast away the const of the component,
+		// since this is a const function.
+		UDesertCharacterMovementComponent* MutableThis = const_cast<UDesertCharacterMovementComponent*>(this);
+
+		MutableThis->ClientPredictionData = new FNetworkPredictionData_Client_Desert(*this);
+		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+		MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+	}
+	
+	return ClientPredictionData;
+}
+
+void UDesertCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+	
+	Safe_bWantsToSprint = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+}
+
+UDesertCharacterMovementComponent::UDesertCharacterMovementComponent()
+{
+	
+}
 
--- Source/DesertSlide/Movement/DesertCharacterMovementComponent.h
+++ Source/DesertSlide/Movement/DesertCharacterMovementComponent.h
@@ -14,8 +14,37 @@ class DESERTSLIDE_API UDesertCharacterMovementComponent : public UCharacterMovem
 {
 	GENERATED_BODY()
 
-protected:
-	virtual void InitializeComponent() override;
+	class FSavedMove_Desert : public FSavedMove_Character
+	{
+		typedef FSavedMove_Character Super;
 
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+		uint8 Saved_bWantsToSprint:1;
+
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		virtual void Clear() override;
+		virtual uint8 GetCompressedFlags() const override;
+		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
+		virtual void PrepMoveFor(ACharacter* C) override;
+	};
+
+	class FNetworkPredictionData_Client_Desert : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FNetworkPredictionData_Client_Desert(const UCharacterMovementComponent& ClientMovement);
+
+		typedef FNetworkPredictionData_Client_Character Super;
+
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+
+	bool Safe_bWantsToSprint;
+	
+public:
+	virtual FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+protected:
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+
+public:
+	UDesertCharacterMovementComponent();
 };
